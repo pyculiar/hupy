0e21ae8abc1dcc6785e993b2696225661a70cf5b
pyculiar <pyculiar@pm.me>
2024-12-26 13:07:18 +0200

    feat: add text node and main

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode(
+            text="This is a text node",
+            text_type=TextType.BOLD,
+            url="https://www.boot.dev"
+    )
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
